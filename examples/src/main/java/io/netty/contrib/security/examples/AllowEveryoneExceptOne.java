57c0107e28973dcb0040930ff30137bc36682bbd
Aayush Atharva <aayush@shieldblaze.com>
2022-08-15 21:09:03 +0530

    Add examples, WIP...

--- examples/src/main/java/io/netty/contrib/security/examples/AllowEveryoneExceptOne.java
+++ examples/src/main/java/io/netty/contrib/security/examples/AllowEveryoneExceptOne.java
@@ -25,90 +25,83 @@ import io.netty.contrib.security.core.Table;
 import io.netty.contrib.security.core.standards.StandardFilter;
 import io.netty.contrib.security.core.standards.StandardNetworkHandler;
 import io.netty.contrib.security.core.standards.StandardRule;
-import io.netty.contrib.security.core.standards.StandardRuleBuilder;
 import io.netty.contrib.security.core.standards.StandardTable;
 import io.netty.contrib.security.core.standards.StandardTables;
 import io.netty5.bootstrap.ServerBootstrap;
-import io.netty5.channel.Channel;
 import io.netty5.channel.ChannelHandlerContext;
 import io.netty5.channel.ChannelInitializer;
 import io.netty5.channel.EventLoopGroup;
-import io.netty5.channel.IoHandle;
-import io.netty5.channel.IoHandler;
-import io.netty5.channel.IoHandlerFactory;
 import io.netty5.channel.MultithreadEventLoopGroup;
 import io.netty5.channel.SimpleChannelInboundHandler;
 import io.netty5.channel.nio.NioHandler;
-import io.netty5.channel.socket.ServerSocketChannel;
 import io.netty5.channel.socket.SocketChannel;
 import io.netty5.channel.socket.nio.NioServerSocketChannel;
 
 import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.UnknownHostException;
 
 public final class AllowEveryoneExceptOne {
 
-    private static EventLoopGroup EVENT_LOOP;
-    private static Channel channel;
-
     public static void main(String[] args) throws Exception {
-        StandardRule rule = StandardRule.newBuilder()
-                .withAction(Action.REJECT)
-                .withSourceIpAddresses(IpAddresses.ACCEPT_ANY)
-                .withSourcePorts(Ports.ANY_PORT)
-                .withDestinationPorts(Ports.ANY_PORT)
-                .withDestinationIpAddress(IpAddresses.ACCEPT_ANY)
-                .withProtocol(Protocol.TCP)
-                .build();
+        EventLoopGroup eventLoopGroup = new MultithreadEventLoopGroup(NioHandler.newFactory());
+        try {
+            StandardRule rule = StandardRule.newBuilder()
+                    .withAction(Action.REJECT)
+                    .withSourceIpAddresses(IpAddresses.create(StaticIpAddress.of("127.0.0.2")))
+                    .withSourcePorts(Ports.ANY_PORT)
+                    .withDestinationPorts(Ports.ANY_PORT)
+                    .withDestinationIpAddress(IpAddresses.ACCEPT_ANY)
+                    .withProtocol(Protocol.TCP)
+                    .build();
 
-        Table table = StandardTable.of(1, "SimpleTable");
-        table.unlock();
-        table.addRule(rule);
-        table.lock();
+            Table table = StandardTable.of(1, "SimpleTable");
+            table.unlock();
+            table.addRule(rule);
+            table.lock();
 
-        StandardTables tables = StandardTables.create();
-        tables.addTable(table);
+            StandardTables tables = StandardTables.create();
+            tables.addTable(table);
 
-        Filter filter = new StandardFilter(tables, Action.REJECT);
-        StandardNetworkHandler networkHandler = new StandardNetworkHandler(filter);
+            Filter filter = new StandardFilter(tables, Action.ACCEPT);
+            StandardNetworkHandler networkHandler = new StandardNetworkHandler(filter);
 
-        EVENT_LOOP = new MultithreadEventLoopGroup(NioHandler.newFactory());
-        ServerBootstrap serverBootstrap = new ServerBootstrap()
-                .group(EVENT_LOOP)
-                .channel(NioServerSocketChannel.class)
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    protected void initChannel(SocketChannel socketChannel) {
-                        socketChannel.pipeline().addFirst(networkHandler);
-                        socketChannel.pipeline().addLast(new SimpleChannelInboundHandler<>() {
-                            @Override
-                            public void channelActive(ChannelHandlerContext ctx) {
-                                System.out.println("Channel Active: " + ctx);
-                            }
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(eventLoopGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) {
+                            socketChannel.pipeline().addFirst(networkHandler);
+                            socketChannel.pipeline().addLast(new SimpleChannelInboundHandler<>() {
+                                @Override
+                                public void channelActive(ChannelHandlerContext ctx) {
+                                    throw new IllegalStateException("This channel should never be active");
+                                }
 
-                            @Override
-                            protected void messageReceived(ChannelHandlerContext ctx, Object o) {
-                                System.out.println("Received Message from: " + ctx.channel().remoteAddress());
-                            }
-                        });
-                    }
-                });
+                                @Override
+                                protected void messageReceived(ChannelHandlerContext ctx, Object o) {
+                                    System.out.println("Received Message from: " + ctx.channel().remoteAddress());
+                                }
+                            });
+                        }
+                    });
 
-        channel = serverBootstrap.bind(9110).asStage().get();
+            serverBootstrap.bind(9110).asStage().get();
 
-        try (Socket socket = new Socket("127.0.0.1", 9110, InetAddress.getByName("127.0.0.2"), 0)) {
-            if (socket.isConnected()) {
-                System.out.println("Socket Connected");
+            try (Socket socket = new Socket("127.0.0.1", 9110, InetAddress.getByName("127.0.0.2"), 0)) {
+                if (socket.isConnected()) {
+                    System.out.println("Socket Connected");
+                } else {
+                    System.out.println("Connection not established yet");
+                }
+
+                Thread.sleep(1000 * 5); // Wait for 5 seconds before checking connection status
+
+                assert socket.isClosed() : "SocketState";
+                System.out.println("Socket Closed");
             }
-        } catch (Exception ex) {
-            if (ex instanceof SocketException) {
-                ex.printStackTrace();
-            } else {
-                System.err.println(ex.getMessage());
-            }
+        } finally {
+            eventLoopGroup.shutdownGracefully();
         }
     }
 }
